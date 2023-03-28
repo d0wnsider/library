9ab6a3f0e63f9e148122f2300046484ab5eaa251
d0wnsider <jmhandsamson@gmail.com>
2023-03-28 00:24:15 -0400

    Add form function to add new book from user input

--- library.js
+++ library.js
@@ -1,7 +1,11 @@
 const cardContainer = document.querySelector('.card-container');
 const form = document.querySelector('.form');
 const addButton = document.querySelector('.add-button');
-
+const submit = document.querySelector('.submit');
+const userTitle = document.querySelector('#title');
+const userAuthor = document.querySelector('#author');
+const userPages = document.querySelector('#pages');
+const userRead = document.querySelector('#read');
 const library = [];
 
 function Book(title, author, pages, read) {
@@ -12,10 +16,10 @@ function Book(title, author, pages, read) {
 }
 
 function addBookToLibrary() {
-  const book1 = new Book("Can't Hurt Me", 'David Goggins', '300', 'Yes');
-  const book2 = new Book('Atomic Habits', 'James Clear', '250', 'Yes');
-  const book3 = new Book('Meditations', 'Marcus Aurelius', '350', 'Yes');
-  library.push(book1, book2, book3);
+  // const book1 = new Book("Can't Hurt Me", 'David Goggins', '300', 'Yes');
+  // const book2 = new Book('Atomic Habits', 'James Clear', '250', 'Yes');
+  // const book3 = new Book('Meditations', 'Marcus Aurelius', '350', 'Yes');
+  // library.push(book1, book2, book3);
 }
 
 function display() {
@@ -42,9 +46,23 @@ function display() {
   });
 }
 
+function submitForm(e) {
+  const titleValue = userTitle.value;
+  const authorValue = userAuthor.value;
+  const pagesValue = userPages.value;
+  const readValue = userRead.value;
+  const book = new Book(titleValue, authorValue, pagesValue, readValue);
+  library.push(book);
+  display();
+  form.style.display = 'none';
+  e.preventDefault();
+}
+
 addButton.addEventListener('click', function (e) {
   form.style.display = 'block';
 });
+
+submit.addEventListener('click', submitForm, false);
 
 addBookToLibrary();
 display();
