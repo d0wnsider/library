7f900e70e4dca8ef75a3c7e84d9ce79289d6d8c6
d0wnsider <jmhandsamson@gmail.com>
2023-03-27 22:09:08 -0400

    Update: displays each book in a card box and fyi this is so hard finally I got it

--- library.js
+++ library.js
@@ -1,6 +1,6 @@
-const addButton = document.querySelector('.add');
+const cardContainer = document.querySelector('.card-container');
 
-let library = [];
+const library = [];
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -10,16 +10,35 @@ function Book(title, author, pages, read) {
 }
 
 function addBook() {
-  addButton.addEventListener('click', () => {
-    const inputTitle = prompt('Title: ');
-    const inputAuthor = prompt('Author: ');
-    const inputPages = prompt('Pages: ');
-    const inputRead = prompt('Read: ');
+  const book1 = new Book("Can't Hurt Me", 'David Goggins', '300', 'Yes');
+  const book2 = new Book('Atomic Habits', 'James Clear', '250', 'Yes');
+  const book3 = new Book('Meditations', 'Marcus Aurelius', '350', 'Yes');
+  library.push(book1, book2, book3);
+}
 
-    const book = new Book(inputTitle, inputAuthor, inputPages, inputRead);
-    library.push(book);
-    console.log(library);
+function display() {
+  library.forEach((book) => {
+    const card = document.createElement('div');
+    card.classList = 'card';
+    const title = document.createElement('p');
+    title.textContent = `Title: ${book.title}`;
+
+    const author = document.createElement('p');
+    author.textContent = `Author: ${book.author}`;
+
+    const pages = document.createElement('p');
+    pages.textContent = `Pages: ${book.pages}`;
+
+    const read = document.createElement('p');
+    read.textContent = `Read: ${book.read}`;
+
+    card.appendChild(title);
+    card.appendChild(author);
+    card.appendChild(pages);
+    card.appendChild(read);
+    cardContainer.appendChild(card);
   });
 }
 
 addBook();
+display();
