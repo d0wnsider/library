b2852fc1a1faa5f6d41ac927f3d064f5d3a3d474
d0wnsider <jmhandsamson@gmail.com>
2023-03-29 10:34:46 -0400

    Refactor functions

--- library.js
+++ library.js
@@ -1,12 +1,7 @@
-const cardContainer = document.querySelector('.card-container');
 const form = document.querySelector('.form');
 const addButton = document.querySelector('.add-button');
 const submit = document.querySelector('.submit');
-const userTitle = document.querySelector('#title');
-const userAuthor = document.querySelector('#author');
-const userPages = document.querySelector('#pages');
-const userRead = document.querySelector('#read');
-const remButton = document.querySelector('.remove-button');
+
 const library = [];
 
 function Book(title, author, pages, read) {
@@ -17,6 +12,19 @@ function Book(title, author, pages, read) {
 }
 
 function addBookToLibrary() {
+  const userTitle = document.querySelector('#title').value;
+  const userAuthor = document.querySelector('#author').value;
+  const userPages = document.querySelector('#pages').value;
+  const userRead = document.querySelector('#read').value;
+
+  const book = new Book(userTitle, userAuthor, userPages, userRead);
+  library.push(book);
+}
+// clear display first before looping again
+function displayLibrary() {
+  const cardContainer = document.querySelector('.card-container');
+  cardContainer.textContent = ``;
+
   library.forEach((book) => {
     const card = document.createElement('div');
     card.classList = 'card';
@@ -35,8 +43,10 @@ function addBookToLibrary() {
 
     const rem = document.createElement('button');
     rem.classList = 'remove-button';
-    rem.setAttribute('type', 'button');
-    rem.textContent = 'X';
+    rem.textContent = `X`;
+    rem.addEventListener('click', (e) => {
+      console.log('1');
+    });
 
     card.appendChild(title);
     card.appendChild(author);
@@ -46,36 +56,14 @@ function addBookToLibrary() {
     cardContainer.appendChild(card);
   });
 }
-// how to remove current display before updating to the latest display?
-function removeDisplay() {
-  library.forEach((book) => {
-    const card = document.querySelector('.card');
-    card.remove();
-  });
-}
 
-addButton.addEventListener('click', function (e) {
+addButton.addEventListener('click', (e) => {
   form.style.display = 'block';
 });
 
-submit.addEventListener('click', e => {
+submit.addEventListener('click', (e) => {
   e.preventDefault();
-  removeDisplay();
-  const titleValue = userTitle.value;
-  const authorValue = userAuthor.value;
-  const pagesValue = userPages.value;
-  const readValue = userRead.value;
-  const book = new Book(titleValue, authorValue, pagesValue, readValue);
-  library.push(book);
   addBookToLibrary();
+  displayLibrary();
   form.style.display = 'none';
 });
-
-// try make a function that loops and ands eventlistener to all buttons everytime it is invoke
-if(remButton) { // gives error because there is no button in html in first load I think
-  remButton.addEventListener('click', function (e) {
-  // how to choose specific object when deleting?
-  console.log('1');
-});
-}
-// addBookToLibrary();
