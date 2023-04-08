fb89daf3318e87dc23712149c0783e5f46e586bb
d0wnsider <jmhandsamson@gmail.com>
2023-04-08 15:31:22 -0400

    Refactor factor functions to class

--- library.js
+++ library.js
@@ -5,11 +5,13 @@ const body = document.querySelector('body');
 
 const library = [];
 
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
 }
 
 function addBookToLibrary() {
